f6f2a107014fe83e2364e59dbe2721c3f1c523ad
draganaMisic0 <misicdragana98@gmail.com>
2025-05-13 13:37:08 +0200

    working with layout

--- employee-app/src/app/layout/main-layout/main-layout.component.ts
+++ employee-app/src/app/layout/main-layout/main-layout.component.ts
@@ -1,31 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MaterialModule } from '../../material/material-imports';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { map, Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [MaterialModule, RouterOutlet, ],
+  imports: [MaterialModule, RouterOutlet, RouterLink, AsyncPipe],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css'
 })
 export class MainLayoutComponent implements OnInit{
 
-  isMobile!: Observable<boolean>;  // Detect screen size for mobile view
+  isMobile$!: Observable<boolean>;  // Detect screen size for mobile view
+  username: string = 'John Doe';  // Just for demo purposes
+  firstName: string = 'John';
+  lastName: string = 'Doe';
+  profilePicUrl: string = 'path/to/profile/pic.jpg';  // Provide your profile image URL
+  private router!: Router;
 
   constructor(
-    private breakpointObserver: BreakpointObserver
-  ){}
+    private breakpointObserver: BreakpointObserver,
+  ){
+    this.router = inject(Router);
+  }
 
   ngOnInit(): void {
-      this.isMobile = this.breakpointObserver.observe([Breakpoints.XSmall]).pipe(
+      this.isMobile$ = this.breakpointObserver.observe([Breakpoints.XSmall]).pipe(
       map(result => result.matches)
     );
   }
 
   logout(): void {
     localStorage.removeItem('userData');
+    this.router.navigate(['/login']);
   }
 
 }
